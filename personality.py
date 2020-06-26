a7e18ce038378b22014f108ed852a89639bdbc2a
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-06-26 09:49:48 +0530

    echo

---
+++ personality.py
@@ -0,0 +1,29 @@
+# Import the random module
+import random
+
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary containing a list of responses for each message
+responses = {
+  "what's your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+
+# Use random.choice() to choose a matching response
+def respond(message):
+    if message in responses:
+        bot_message = random.choice(responses[message])
+    else:
+        bot_message = random.choice(responses["default"])
+    return bot_message
+a=input()
+print(respond(a))
